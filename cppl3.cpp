bd6ca5224d7eea41e016c02fede9f95c5044f77f
Paul Standley <paulstandley1972@gmail.com>
2020-05-06 18:17:58 +0100

    std cin ingore 32767

--- cppl3.cpp
+++ cppl3.cpp
@@ -33,7 +33,20 @@ void cinValueAlsoCapturesNewline()
 {
     std::cout << "Pick 1 of 2: ";
     int choise{};
-    std::cin >> choise;//cin not only captures the value, it also captures the newline
+    std::cin >> choise;
+    //cin not only captures the value, it also captures the newline
+
+    std::cin.ignore(32767, '\n');
+    // ignore up to 32767 characters until a \n is removed
+    /*
+    That tells std::cin.ignore() how many characters to ignore up to.
+    We picked that number because it’s the largest signed value guaranteed
+    to fit in a (2-byte) integer on all platforms.
+
+    #include <limits>
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+    ignore unlimited characters until a \n is removed
+    */
 
     std::cout << "Now enter your name: ";
     std::string name{};
